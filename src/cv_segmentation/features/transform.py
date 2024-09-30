b2805665f4be0e082f5129d7144bfbbd023217a5
YaninaK <kutovaiayp@yahoo.com>
2024-09-30 05:29:40 +0300

    Add transform.py

---
+++ src/cv_segmentation/features/transform.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def data_transform(sample: dict):
+    """
+    Apply horizontal and vertical flips with 50% probability
+    """
+    image = sample["image"]
+    mask = sample["mask"]
+
+    if np.random.random() > 0.5:
+        image = np.fliplr(image)  # Horizontal flip
+        mask = np.fliplr(mask)
+
+    if np.random.random() > 0.5:
+        image = np.flipud(image)  # Vertical flip
+        mask = np.flipud(mask)
+
+    sample = {"image": image, "mask": mask}
+
+    return sample

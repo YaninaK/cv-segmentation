09e61109b608245a59be9ce3b75cbeb2e088dd33
YaninaK <kutovaiayp@yahoo.com>
2024-10-03 02:03:04 +0300

    Reformated make_dataset.py

--- src/cv_segmentation/data/make_dataset.py
+++ src/cv_segmentation/data/make_dataset.py
@@ -13,31 +13,15 @@ logger = logging.getLogger(__name__)
 __all__ = ["load_and_clean_data"]
 
 
-def load_data(config: Optional[dict] = None) -> Tuple[pd.DataFrame, list]:
+def load_data(config: Optional[dict] = None) -> Tuple[pd.DataFrame, list[np.array]]:
     if config is None:
         config = CONFIG
 
     y_train = pd.read_csv(config["csv_file"], index_col=0)
 
-    file_names = os.listdir(config["root_dir"])
     images_train = []
-    for img_name in tqdm(file_names):
-        img = np.load(os.path.join(config["root_dir"], img_name))
+    for index in tqdm(y_train.index):
+        img = np.load(os.path.join(config["root_dir"], f"{index}.npy"))
         images_train.append(img)
 
     return y_train, images_train
-
-
-def clean_outliers(
-    y_train: pd.DataFrame, images_train: list, config: Optional[float] = None
-) -> Tuple[pd.DataFrame, np.array]:
-
-    if config is None:
-        config = CONFIG
-
-    image_arr = np.reshape(np.asarray(images_train), (len(images_train), -1))
-    image_arr_min = np.nan_to_num(image_arr).min(axis=1)
-
-    y_train = y_train.iloc[image_arr_min > config["threshold"]].reset_index()
-
-    return y_train, image_arr[image_arr_min > config["threshold"]]

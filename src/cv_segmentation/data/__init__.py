045e91d646e5833c8d63d556eca843f2494961bb
YaninaK <kutovaiayp@yahoo.com>
2024-10-02 19:34:06 +0300

    Update wells distribution in data\__init__.py

--- src/cv_segmentation/data/__init__.py
+++ src/cv_segmentation/data/__init__.py
@@ -3,7 +3,7 @@ CONFIG = {
     "root_dir": "/content/cv-segmentation/data/raw/images/",
     "save_dir": "../data/interim/",
     "threshold": -0.25,
-    "train": [6, 7, 8, 9, 11, 12, 13, 14],
-    "val": [3, 10, 15],
-    "test": [1, 2, 4, 5],
+    "train": [1, 6, 7, 8, 10, 11, 12, 13, 14],
+    "val": [3, 9],
+    "test": [2, 4, 5, 15],
 }
